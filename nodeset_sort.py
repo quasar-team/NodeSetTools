882c5d1b469c5f1e38414b1f3a3c261bbfd89290
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2021-07-08 17:16:59 +0200

    added sorter

---
+++ nodeset_sort.py
@@ -0,0 +1,22 @@
+import argparse
+from lxml import etree
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input')
+    parser.add_argument('output')
+
+    args = parser.parse_args()
+
+    tree = etree.parse(args.input)
+
+    def sortchildrenby(parent, attr):
+        parent[:] = sorted(parent, key=lambda child: child.get(attr))
+
+    sortchildrenby(tree.getroot(), 'NodeId')
+
+    tree.write(args.output, encoding='utf-8', xml_declaration=True)
+
+if __name__ == "__main__":
+    main()
