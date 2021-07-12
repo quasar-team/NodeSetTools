920b848f7ecd6f8c0dfd98652dd73e206476fc04
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2021-07-12 11:46:18 +0200

    checking for attr values works

--- nodeset_compare.py
+++ nodeset_compare.py
@@ -4,6 +4,9 @@ from lxml import etree
 import argparse
 import pdb
 import sys
+import logging
+
+from colorama import Fore, Style
 
 NAMESPACES = {'x':'http://opcfoundation.org/UA/2011/03/UANodeSet.xsd'}
 
@@ -12,21 +15,36 @@ def etree_from_filename(fn):
     return etree.parse(file)
 
 def nodeid_scan(entity_type, reference_tree, test_tree):
-    """entity_type to be anything that has NodeId, so any descendant of UANode, e.g. UAVariable, UAObject..."""
+    '''Scans all entity_type in reference tree and xvalidates in test_tree
+    entity_type to be anything that has NodeId, so any descendant of UANode, e.g. UAVariable, UAObject...'''
     any_failure = False
     for ref_entity in reference_tree.xpath(f'x:{entity_type}', namespaces=NAMESPACES):
         ref_nodeid = ref_entity.attrib["NodeId"]
-        #print(f'At: {ref_nodeid}')
-        # get the object at test.
+        logging.debug(f'At: {ref_nodeid}')
         xpath_expr = f"x:{entity_type}[@NodeId='{ref_nodeid}']"
         q = test_tree.xpath(xpath_expr, namespaces=NAMESPACES)
         if len(q) != 1:
-            print(f'Error: entity missing in test_tree. Type: {entity_type}, NodeId: {ref_nodeid}')
+            print(f'Error: entity in test_tree. Type: {entity_type}, NodeId: {ref_nodeid}')
             any_failure = True
+        test_element = q[0]
+        if test_element != ref_entity:
+            # let's see why they are different.
+            if test_element.attrib != ref_entity.attrib:
+                for attr in ref_entity.attrib:
+                    try:
+                        if test_element.attrib[attr] != ref_entity.attrib[attr]:
+                            print(f"reference attribute value fail, attribute{attr} ref_value{ref_entity.attrib[attr]} test_value{test_element.attrib[attr]}")
+                            any_failure = True
+                    except KeyError:
+                        print(f'{Fore.RED}Attribute {Fore.YELLOW}{attr}{Fore.RED} is missing in test entity{Style.RESET_ALL}(at nodeid: {ref_nodeid})')
+                        any_failure = True
+            #pdb.set_trace()
     return any_failure
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
+
     parser = argparse.ArgumentParser()
     parser.add_argument("reference")
     parser.add_argument("test")
@@ -38,8 +56,8 @@ def main():
     test_tree = etree_from_filename(args.test)
 
     any_failure = False
-    
-    NodeIdEntities = ['UAObject', 'UAVariable']
+
+    NodeIdEntities = ['UAObject', 'UAVariable', 'UAMethod']
     for entity_type in NodeIdEntities:
         fail = nodeid_scan(entity_type, reference_tree, test_tree)
         any_failure = any_failure or fail
